ea87162a108b5b5b3612bdbbb25827ca4ddcdfb2
Keyeric <keyeric.bristol@gmail.com>
2020-02-03 13:06:10 -0700

    META tagsgit add .!

--- posthere/src/App.js
+++ posthere/src/App.js
@@ -1,26 +1,9 @@
 import React from "react";
-import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
-import { makeStyles } from '@material-ui/styles';
-import { Container } from '@material-ui/core';
-
-import Login from "./components/Login";
-import PrivateRoute from "./components/PrivateRoute";
 import './App.css';
 
 function App() {
   return (
-    <Router>
-      <div className="App">
-        {/* 
-          Build a PrivateRoute component that will 
-          display BubblePage when you're authenticated 
-        */}
-        <Switch>
-          <PrivateRoute path="/protected" />
-          <Route exact path="/" component={Login} />
-        </Switch>
-      </div>
-    </Router>
+    null
   );
 }
 
